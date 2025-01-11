1548beac1b70fe4c2d3e6adf77b581a8960e42e2
Tal Jacob <taljacob2@gmail.com>
2025-01-11 14:27:20 +0200

    Add Images To All Restaurants
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- app/src/main/java/com/example/dineDelight/repositories/RestaurantRepository.kt
+++ app/src/main/java/com/example/dineDelight/repositories/RestaurantRepository.kt
@@ -10,7 +10,7 @@ object RestaurantRepository {
             name = "Italian Delight",
             rating = 4.5f,
             availableSlots = listOf("18:00", "19:00", "20:00"),
-            imageUrl = "https://s6.ezgif.com/tmp/ezgif-6-6ddeaf195d.jpg",
+            imageUrl = "https://t3.ftcdn.net/jpg/06/07/90/92/360_F_607909283_b3ysd6mRICNihTjLwCENgTwP08Zb4koz.jpg",
             area = "Italian",
             description = "Eat the best pasta in town"
         ),
@@ -28,7 +28,7 @@ object RestaurantRepository {
             name = "Canadian Bistro",
             rating = 4.2f,
             availableSlots = listOf("12:00", "13:00", "14:00"),
-            imageUrl = "https://example.com/canadian.jpg",
+            imageUrl = "https://media.cntraveler.com/photos/5b22cabaf0cc9956e5adca3c/16:9/w_1920%2Cc_limit/Bar-Raval_36361674480_70a3ef47c9_o.jpg",
             area = "Canadian",
             description = "Taste the flavors of Canada"
         ),
@@ -37,7 +37,7 @@ object RestaurantRepository {
             name = "American Grill",
             rating = 4.6f,
             availableSlots = listOf("17:00", "18:00", "19:00"),
-            imageUrl = "https://example.com/american.jpg",
+            imageUrl = "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/5a/c0/04/diner-americano-in-stile.jpg",
             area = "American",
             description = "Enjoy classic American dishes"
         ),
@@ -46,7 +46,7 @@ object RestaurantRepository {
             name = "Mexican Fiesta",
             rating = 4.7f,
             availableSlots = listOf("16:00", "17:00", "18:00"),
-            imageUrl = "https://example.com/mexican.jpg",
+            imageUrl = "https://margs.com/wp-content/uploads/2024/06/Margs-Woodbridge-Interior.jpg",
             area = "Mexican",
             description = "Spice up your meal with Mexican cuisine"
         ),
@@ -55,7 +55,7 @@ object RestaurantRepository {
             name = "French Elegance",
             rating = 4.9f,
             availableSlots = listOf("19:00", "20:00", "21:00"),
-            imageUrl = "https://example.com/french.jpg",
+            imageUrl = "https://thelagirl.com/wp-content/uploads/2017/07/French-Restaurants-in-Los-Angeles-1.jpg",
             area = "French",
             description = "Experience the taste of France"
         ),
@@ -64,7 +64,7 @@ object RestaurantRepository {
             name = "Indian Spice",
             rating = 4.4f,
             availableSlots = listOf("18:30", "19:30", "20:30"),
-            imageUrl = "https://example.com/indian.jpg",
+            imageUrl = "https://images.axios.com/lIPBPJ802rSLL98aIQ9FDkQd2Go=/0x0:6687x3761/1920x1080/2023/04/11/1681223212336.jpg",
             area = "Indian",
             description = "Savor the rich flavors of India"
         ),
@@ -73,7 +73,7 @@ object RestaurantRepository {
             name = "Greek Taverna",
             rating = 4.3f,
             availableSlots = listOf("12:30", "13:30", "14:30"),
-            imageUrl = "https://example.com/greek.jpg",
+            imageUrl = "https://www.greektastebeyondborders.com/wp-content/uploads/2020/10/oia-greek-taverna-2.jpg",
             area = "Greek",
             description = "Enjoy traditional Greek dishes"
         ),
@@ -82,7 +82,7 @@ object RestaurantRepository {
             name = "Moroccan Delight",
             rating = 4.5f,
             availableSlots = listOf("17:00", "18:00", "19:00"),
-            imageUrl = "https://example.com/moroccan.jpg",
+            imageUrl = "https://www.lesjardinsdelakoutoubia.com/en/img/slideshow_xxlarge/524_restaurant-marocain.jpeg",
             area = "Moroccan",
             description = "Discover the flavors of Morocco"
         )
