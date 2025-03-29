11d4a307491e6e7202c4f83e1b1c2044d4225102
Tal Jacob <taljacob2@gmail.com>
2025-03-29 22:42:18 +0300

    Add Veronica Font

--- app/src/main/java/com/example/dineDelight/ui/theme/Type.kt
+++ app/src/main/java/com/example/dineDelight/ui/theme/Type.kt
@@ -2,19 +2,29 @@ package com.example.dineDelight.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.dineDelight.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily(Font(R.font.veronica_script)),
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
+    titleLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.veronica_caps)),
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    // Add more styles as needed
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
